fe0599ffe2e91f01e0aef4d79d81192e277c3ad3
Thomas Collins <tcollins@hubspot.com>
2019-05-25 08:50:16 +0100

    election

---
+++ src/pages/tally.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react"
+
+export default () => {
+  useEffect(() => {
+    window.location.href = 'https://www.dropbox.com/s/ym4wwk0brc74faz/Tally%20-%20Kanturk%20Local%20Election%202019.xlsx?dl=0'
+  }, [])
+  return <p>election</p>
+}
