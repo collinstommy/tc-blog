db664b3d506c967f080e3e265962d79a6ac30de9
Thomas Collins <tomascollins@gmail.com>
2020-02-04 21:46:39 +0000

    add election result link

---
+++ src/pages/election-2020.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react"
+import styled from "styled-components";
+
+const Container = styled.main`
+  padding: 3rem;
+`;
+
+export default () => {
+  return <Container>
+    <h1>Tally Results</h1>
+    <h4>Links will be live on the day of the count.</h4>
+    <a href="">Cork North West</a>
+    </Container>
+}
