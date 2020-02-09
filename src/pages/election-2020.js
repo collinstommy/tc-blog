0e6e2ab4089c0b8966424499c5c5e33f8819ebe5
Thomas Collins <tomascollins@gmail.com>
2020-02-09 09:11:11 +0000

    redirect

--- src/pages/election-2020.js
+++ src/pages/election-2020.js
@@ -8,6 +8,9 @@ const Container = styled.main`
 export default () => {
   return <Container>
     <h1>Tally Results</h1>
-    <a href="https://www.dropbox.com/s/fwhdcvii4rprgxj/General%20Election%202020%20-%20Final.xlsx?dl=0">Cork North West</a>
+    <ul>
+      <li><a href="https://www.dropbox.com/s/fwhdcvii4rprgxj/General%20Election%202020%20-%20Final.xlsx?dl=0">Cork North West</a></li>
+      <li>Cork East (link will be posted at end of 1st count)</li>
+      </ul>
     </Container>
 }
